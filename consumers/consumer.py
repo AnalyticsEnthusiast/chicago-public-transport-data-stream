49b65d4a376b14a75cf54947f5e89be1c1ca6418
Darren Foley <darren.foley@ucdconnect.ie>
2022-01-02 13:17:45 +0000

    Fixed typo, changed "=" to ":"

--- consumers/consumer.py
+++ consumers/consumer.py
@@ -33,7 +33,7 @@ class KafkaConsumer:
         self.broker_properties = {
             "bootstrap.servers": "PLAINTEXT://localhost:9092",
             "group.id": 0,
-            "auto.offset.reset" = "earliest" if self.offset_earliest else "latest"
+            "auto.offset.reset": "earliest" if self.offset_earliest else "latest"
         }
 
         if is_avro is True:
